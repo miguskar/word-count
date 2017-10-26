449ce752de0a43a3ea2254f62775449fd63e1d80
Mikael Karlsson <miguskar@gmail.com>
2017-10-26 09:24:09 +0200

    replace alphanumerics with basically entire latin character range.
    u0021-u007e = common characters
    u00a1-u024f = latin supplement & extended a / b
    u0300-u036f = combining diacritical marks
    u2018-u201f = parts of general punctiation range

--- index.js
+++ index.js
@@ -6,7 +6,7 @@
  * Copyright (c) 2015 - 2016 by Hsiaoming Yang.
  */
 
-var pattern = /[a-zA-Z0-9_\u0392-\u03c9\u00c0-\u00ff\u0600-\u06ff\u0400-\u04FF]+|[\u4e00-\u9fff\u3400-\u4dbf\uf900-\ufaff\u3040-\u309f\uac00-\ud7af]+|[\u00E4\u00C4\u00E5\u00C5\u00F6\u00D6]+/g;
+var pattern = /[\u0021-\u007e\u00A1-\u024f\u0300-\u036f\u0392-\u03c9\u2018-\u201f\u00c0-\u00ff\u0600-\u06ff\u0400-\u04FF]+|[\u4e00-\u9fff\u3400-\u4dbf\uf900-\ufaff\u3040-\u309f\uac00-\ud7af]+|[\u00e4\u00c4\u00e5\u00c5\u00f6\u00d6]+/g;
 
 module.exports = function (data) {
   var m = data.match(pattern);
